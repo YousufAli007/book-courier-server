3efb23c9ababcdf6dbddf52b4624600d9134ff5f
Yousuf Ali <yousuf787700@gmail.com>
2025-12-16 10:10:56 +0600

    install cors

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3000
-
+const cors =require('cors')
+// middleware
+app.use(express.json())
+app.use(cors())
 app.get("/", (req, res) => {
   res.send("welcome to book courier");
 });
