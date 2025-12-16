506c64b814ae5653f998209f6419399fad3ca2ce
Yousuf Ali <yousuf787700@gmail.com>
2025-12-16 10:42:41 +0600

    added book collention

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000
@@ -6,6 +7,57 @@ const cors =require('cors')
 // middleware
 app.use(express.json())
 app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cydeyqc.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database =client.db('book_courier')
+    const bookCollection =database.collection('books')
+
+    // books api
+    app.post('/books', async (req, res)=>{
+      const book =req.body;
+      book.createAT = new Date()
+      const result =await bookCollection.insertOne(query)
+      res.send(result)
+    })
+   app.get("/latest_books", async (req, res) => {
+     const result = await bookCollection
+       .find()
+       .sort({ createdAt: -1 })
+       .limit(6)
+       .toArray();
+
+     res.send(result);
+   });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get("/", (req, res) => {
   res.send("welcome to book courier");
 });
